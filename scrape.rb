ba469f45b087d153605bf324dbb25a89d391cdd0
Sota <sooota09@gmail.com>
2020-07-22 17:26:58 +0900

    [update] news取得、section取得をメソッド化

--- scrape.rb
+++ scrape.rb
@@ -1,4 +1,6 @@
 require 'net/http'
+require 'nokogiri'
+require 'json'
 
 # 対象サイトのHTMLを取得するメソッド
 def get_from(url)
@@ -10,34 +12,24 @@ def write_file(path, text)
   File.open(path, 'w') { |file| file.write(text) }
 end
 
+# newsを取得するメソッド
+def scrape_news(news)
+  {
+    title: news.xpath('./p/strong/a').first.text,
+    url: news.xpath('./p/strong/a').first['href']
+  }
+end
 
-require 'nokogiri'
-
-html = File.open('pitnews.html', 'r') { |f| f.read }
-doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
-
-section = doc.xpath('/html/body/main/section[2]').first
-
-pitnews = []
-doc.xpath('/html/body/main/section').each_with_index do |section, index|
-
-  next if index.zero?
-
-  contents = {category: nil, news: []}
-  contents[:category] = section.xpath('./h6').first.text
-  
-  section.xpath('./div/div').each do |node|
-    title = node.xpath('./p/strong/a').first.text
-    url = node.xpath('./p/strong/a').first['href']
-    
-    news = {title: title, url: url}
-    contents[:news] << news
-  end
-
-  pitnews << contents  
+# sectionを取得するメソッド
+def scrape_section(section)
+  {
+    category: section.xpath('./h6').first.text,
+    news: section.xpath('./div/div').map { |node| scrape_news(node) }
+  }
 end
 
 
-require 'json'
-
-File.open('pitnews.json', 'w') { |file| file.write({pitnews: pitnews}.to_json) }
+html = File.read('pitnews.html')
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+pitnews = doc.xpath('/html/body/main/section[position() > 1]').map { |section| scrape_section(section) }
+write_file('pitnews.json', {pitnews: pitnews}.to_json)
