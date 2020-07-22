46c900196dd834a8ed1cb9f769076fe37123052c
Sota <sooota09@gmail.com>
2020-07-22 15:44:56 +0900

    [delete] 不要ファイル削除

--- file_sample.rb
+++
@@ -1,7 +0,0 @@
-file = File.open('scrape.rb', 'r') # 'r' => ファイルを読み込みモードで開く
-puts file.read
-file.close
-
-file = File.open('hello.txt', 'w') # 'w' => ファイルを書き込みモードで開く　※既に存在するファイルの場合中身を空にしてしまう）
-file.write('Hello World!')
-file.close
