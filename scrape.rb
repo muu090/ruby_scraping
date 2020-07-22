e53245b49860d22afa3a93ebb223704ee8fb1c6e
Sota <sooota09@gmail.com>
2020-07-22 17:02:44 +0900

    [update] XPathを使ったスクレイピング

--- scrape.rb
+++ scrape.rb
@@ -10,8 +10,34 @@ def write_file(path, text)
   File.open(path, 'w') { |file| file.write(text) }
 end
 
-# html = get_from('https://masayuki14.github.io/pit-news/')
-# write_file('pitnews.html', html)
-# ↓上の２行を１行で書くとこうなる
-write_file('pitnews.html', get_from('https://masayuki14.github.io/pit-news/'))
 
+require 'nokogiri'
+
+html = File.open('pitnews.html', 'r') { |f| f.read }
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+
+section = doc.xpath('/html/body/main/section[2]').first
+
+pitnews = []
+doc.xpath('/html/body/main/section').each_with_index do |section, index|
+
+  next if index.zero?
+
+  contents = {category: nil, news: []}
+  contents[:category] = section.xpath('./h6').first.text
+  
+  section.xpath('./div/div').each do |node|
+    title = node.xpath('./p/strong/a').first.text
+    url = node.xpath('./p/strong/a').first['href']
+    
+    news = {title: title, url: url}
+    contents[:news] << news
+  end
+
+  pitnews << contents  
+end
+
+
+require 'json'
+
+File.open('pitnews.json', 'w') { |file| file.write({pitnews: pitnews}.to_json) }
